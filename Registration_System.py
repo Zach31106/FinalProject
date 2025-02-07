509882aaa4a2ff07ec0d901961b383ed393bff58
Zach Landry <zachlandry@Zachs-MacBook-Air.local>
2025-02-07 16:54:55 -0500

    fixed bugs

--- Registration_System.py
+++ Registration_System.py
@@ -1,34 +1,36 @@
 def viewCourses(array):
-    print("The following courses are available for registration:")
     i = 1
     for item in array:
         print(i, item)
         i += 1
-def addCourses(array1, array2): #bug that doesn't properly add a course
+def addCourses(array1, array2):
     number = int(input("Enter the course number you'd like to register for:\n"))
-    array1[number - 1] = array2[number - 1]
+    array1.append(array2[number - 1])
     return array1
-def removeCourses(array1, number):
-    array1.remove(number - 1)
+def removeCourses(array1):
+    print("These are your current courses:")
+    i = 1
+    for item in array1:
+        print(i, item)
+        i += 1
+    number = int(input("Enter the course number you'd like to remove:\n"))
+    array1.remove(array1[number - 1])
     return array1
 def registration():
     availableCourses = ["Course 1", "Course 2", "Course 3", "Course 4", "Course 5"]
     userCourses = []
-    print("It's time to register for classes! To view available courses type 'view'. To register type 'add'. To remove type 'remove'.")
+    print("It's time to register for classes! To view available courses type 'courses'. To register type 'add'. To remove type 'remove'. To see your current courses type 'view'.")
     while True:
         choice = input("What would you like to do?\n")
-        if choice == "view":
+        if choice == "courses":
+            print("The following courses are available for registration:")
             viewCourses(availableCourses)
         elif choice == "add":
             addCourses(userCourses, availableCourses)
         elif choice == "remove":
-            print("These are your current courses:\n")
-            i = 1
-            for item in userCourses:
-                print(i, item)
-                i += 1
-            number = int(input("Enter the course number you'd like to remove:\n"))
-            removeCourses(userCourses, number)
+            removeCourses(userCourses)
+        elif choice == "view":
+            viewCourses(userCourses)
         else:
             print("Invalid choice.")
 # ==============================================================================
