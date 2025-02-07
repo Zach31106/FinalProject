5d47c9997c9eaf01536852d21e17610f6be19c1c
Zach Landry <zachlandry@Zachs-MacBook-Air.local>
2025-02-07 15:08:03 -0500

    Update

--- ToDoList_Basic.py
+++ ToDoList_Basic.py
@@ -8,13 +8,13 @@ def ToDoList():
         "This is a simple to do list. To add a new item, enter 'add'. To remove an item, enter 'remove'. To see all items, enter 'list'.")
     taskList = []
     while True:
-        command = str(input("What would you like to do?\n"))
+        command = input("What would you like to do?\n")
         while True:
-            if command == command1:
+            if command == "add":
                 add = input("What would you like to add?\n")
                 taskList.append(add)
                 break
-            elif command == command2:
+            elif command == "remove":
                 i = 1
                 for item in taskList:
                     print(i, item)
@@ -22,13 +22,13 @@ def ToDoList():
                 remove = int(input("Which numbered task would you like to remove?\n"))
                 taskList.remove(taskList[remove - 1])
                 break
-            elif command == command3:
+            elif command == "list":
                 i = 1
                 for item in taskList:
                     print(i, item)
                     i += 1
                 break
-            elif command != command1 or command != command2 or command != command3:
+            elif command != "add" or command != "remove" or command != "list":
                 print("Invalid input. Please enter 'add', 'remove' or 'list'.")
                 break
 # ==============================================================================
