d2659c7ba05568efdbe600f93f9d5b5ecd2a244f
zachlandry <zacharylandry06@gmail.com>
2025-02-24 18:31:00 -0500

    Flow Chart

--- Testing.py
+++ Testing.py
@@ -1,7 +1,11 @@
 import tkinter as tk
 
-r = tk.Tk()
-r.title('Counting Seconds')
-button = tk.Button(r, text='Stop', width=25, command=r.destroy)
+window = tk.Tk()
+window2 = tk.Tk()
+window.title('Main Window')
+window.geometry('400x600')
+window2.title('Other Window')
+window2.geometry('400x600')
+button = tk.Button(window, text='Stop', width=25, command=window2.destroy)
 button.pack()
-r.mainloop()
+window.mainloop()
